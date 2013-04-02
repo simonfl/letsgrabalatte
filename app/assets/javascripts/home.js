6e4d2d6f53058e2ad4aa1b22a9eaa134d3a89fb7
Stephanie Chen <eighnjel@gmail.com>
2013-04-01 23:48:02 -0700

    hook up form to functions

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,26 +9,7 @@ function get_location_from_response(response) {
 
 // TODO(simon): format/sprintf equivalent?
 var geocoding_url_template = 'https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=';
-var mission = 'mission, sf';
-var marina = 'marina, sf'
-
-var your_location_req = jQuery.get(geocoding_url_template + mission);
-var friends_location_req = jQuery.get(geocoding_url_template + marina);
-
-
-
-var locations = {};
-
 var explore_url_template = 'https://api.foursquare.com/v2/venues/explore?query=coffee&client_id=XOBCADAGYQQDQMRZGQVX5Y4DF3CVCATJUIJEGB5UXQ2PESZZ&client_secret=4GZ31YY4KRJZXMW0O0DSHNAEY4MH0GJNKA0PUEXC4BACO3LQ&v=20130330';
-
-your_location_req.done(function(your_response) {
-  your_location = get_location_from_response(your_response);
-  friends_location_req.done(function(friends_response){
-    friends_location = get_location_from_response(friends_response);
-    place_location_markers(your_location, friends_location);
-    explore(your_location, friends_location);
-  })
-});
 
 function explore(your_location, friends_location) {
   var explore_url = explore_url_template + '&ll=' + your_location.lat + ',' + your_location.lng;
@@ -63,3 +44,22 @@ function place_location_markers(you, friend) {
   });
   
 }
+
+$("#go").click(function() {
+  var your_location = $("#addy1").val();
+  var friend_location = $("#addy2").val();
+
+  var your_location_req = $.get(geocoding_url_template + your_location);
+  var friends_location_req = $.get(geocoding_url_template + friend_location);
+
+  var locations = {};
+
+  your_location_req.done(function(your_response) {
+    your_location = get_location_from_response(your_response);
+    friends_location_req.done(function(friends_response){
+      friends_location = get_location_from_response(friends_response);
+      place_location_markers(your_location, friends_location);
+      explore(your_location, friends_location);
+    })
+  });
+});
