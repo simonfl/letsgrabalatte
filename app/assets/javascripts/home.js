4ddeae1a3a71cc993fef0d8ded652f06e49dfd3c
Simon Favreau-Lessard <simonfl@gmail.com>
2013-04-02 00:26:38 -0700

    Center in between and explore radius

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -11,6 +11,35 @@ function get_location_from_response(response) {
 var geocoding_url_template = 'https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=';
 var explore_url_template = 'https://api.foursquare.com/v2/venues/explore?query=coffee&client_id=XOBCADAGYQQDQMRZGQVX5Y4DF3CVCATJUIJEGB5UXQ2PESZZ&client_secret=4GZ31YY4KRJZXMW0O0DSHNAEY4MH0GJNKA0PUEXC4BACO3LQ&v=20130330';
 
+// From http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+function distance_between_locations_in_m(location1, location2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(location2.lat - location1.lat);
+  var dLon = deg2rad(location2.lng-location1.lng); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(location1.lat)) * Math.cos(deg2rad(location2.lat)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = (R * c) * 1000; // Distance in m
+  return d;
+}
+
+function suggested_radius(location1, location2) {
+  return distance_between_locations_in_m(location1, location2) / 3;
+}
+
+function middle_point(location1, location2) {
+  var location = []
+  location.lat = (location1.lat + location2.lat) / 2;
+  location.lng = (location1.lng + location2.lng) / 2;
+  return location;
+};
+
 
 function add_to_map(name, location) {
   new google.maps.Marker({
@@ -27,11 +56,13 @@ function add_explore_result_to_map(index, item) {
   add_to_map(name, location);
 };
 
-function explore(your_location, friends_location) {
-  var explore_url = explore_url_template + '&ll=' + your_location.lat + ',' + your_location.lng;
-    jQuery.get(explore_url, function(response) {
-	jQuery.each(response.response.groups[0].items, add_explore_result_to_map);
-    });
+function explore(center, your_location, friends_location) {
+  var radius = suggested_radius(your_location, friends_location);
+  var explore_url = explore_url_template + '&ll=' + center.lat + ',' + center.lng + '&radius=' + radius;
+
+  jQuery.get(explore_url, function(response) {
+    jQuery.each(response.response.groups[0].items, add_explore_result_to_map);
+  });
 };
 
 function initialize() {
@@ -61,14 +92,13 @@ $("#go").click(function() {
   var your_location_req = $.get(geocoding_url_template + your_location);
   var friends_location_req = $.get(geocoding_url_template + friend_location);
 
-  var locations = {};
-
   your_location_req.done(function(your_response) {
     your_location = get_location_from_response(your_response);
     friends_location_req.done(function(friends_response){
       friends_location = get_location_from_response(friends_response);
+      var center = middle_point(your_location, friends_location);
       place_location_markers(your_location, friends_location);
-      explore(your_location, friends_location);
+      explore(center, your_location, friends_location);
     })
   });
 });
