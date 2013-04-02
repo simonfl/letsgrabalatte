98c713c333eb9c66cbc298281965b6c6d63f0a48
Stephanie Chen <eighnjel@gmail.com>
2013-04-01 23:28:20 -0700

    add map (hardcoded center, etc)

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+var map;
+
 function get_location_from_response(response) {
   return response.results[0].geometry.location;
 };
@@ -23,6 +25,7 @@ your_location_req.done(function(your_response) {
   your_location = get_location_from_response(your_response);
   friends_location_req.done(function(friends_response){
     friends_location = get_location_from_response(friends_response);
+    place_location_markers(your_location, friends_location);
     explore(your_location, friends_location);
   })
 });
@@ -31,3 +34,32 @@ function explore(your_location, friends_location) {
   var explore_url = explore_url_template + '&ll=' + your_location.lat + ',' + your_location.lng;
   jQuery.get(explore_url);
 };
+
+$(document).ready(initialize);
+
+function initialize() {
+  // TODO: replace center with calculated center
+  var mapOptions = {
+    center: new google.maps.LatLng(37.777499,-122.418594),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
+}
+
+function place_location_markers(you, friend) {
+  var you_marker = new google.maps.Marker({
+    position: new google.maps.LatLng(you.lat, you.lng),
+    map: map,
+    title: 'You'
+  });
+  
+  var friend_marker = new google.maps.Marker({
+    position: new google.maps.LatLng(friend.lat, friend.lng),
+    map: map,
+    title: 'Your friend'
+  });
+  
+}
