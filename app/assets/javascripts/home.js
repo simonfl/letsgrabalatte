d7e3bdb615b78016f5066b35ec353e5b9a0c8484
Simon Favreau-Lessard <simonfl@gmail.com>
2013-04-02 00:57:24 -0700

    More google analytics tweaks

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -119,6 +119,7 @@ $("#again").click(function(){
 // Google Analytics
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-39789834-1']);
+_gaq.push(['_setDomainName', 'letsgrabalatte.com']);
 _gaq.push(['_trackPageview']);
 
 (function() {
@@ -126,4 +127,3 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
-
